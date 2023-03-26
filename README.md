1aa7cb8c4a7f0b31386021380f73e41f33dd0e55
anshyyy <anshuman9998@gmail.com>
2023-03-26 23:55:56 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# pockey
+
+## A app that lets you track of expenses
+
---
+++ android/app/src/main/kotlin/com/example/pockey/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.pockey
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
